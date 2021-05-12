159538f7e5449b90d31fbb2439d39fc1108ada98
Ajith.KumarReddy <Ajith.KumarReddy@IN2270990W1.ey.net>
2021-05-12 22:41:37 +0530

    Welcome page and login page with post and get methods

--- spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/controller/LoginController.java
+++ spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/springbootfirstwebapplication/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.in28minutes.springboot.web.springbootfirstwebapplication.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,17 +13,22 @@ import org.springframework.web.servlet.ModelAndView;
 //Annotation to create a controller. This is required. jave will pick up request in here as requist mapping
 @Controller 
 public class LoginController {
+	//send Request parms from url as parameters in function
 	//public String loginMessage(@RequestParam String name, ModelMap model) {
+	
 	// RequestMapping Annotation to map URL TO METHOD
-	@RequestMapping(value = "/login")
-	public ModelAndView loginMessage(@Request) {
-		//model.put("name", name);
-		ModelMap model = new ModelMap();
-		return new ModelAndView("login", model);
-		
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String showLoginPage(ModelMap model) {
+		//return new ModelAndView("login", model);
+		return "login";	
+	}
+	@RequestMapping(value = "/login" , method = RequestMethod.POST)
+	public String showWelcomePage(ModelMap model, @RequestParam String name) {
+		model.put("name", name);
+		return "welcome";		
 	}
 }
+
 //@ResponseBody
 //Return the string directly instead of looking for a vieworjsp
-
 //@RequestParam- take parameter from url
