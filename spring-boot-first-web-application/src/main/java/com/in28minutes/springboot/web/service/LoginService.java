2031c5d31dfc3391d188f9170a7a0d3ad7fe8789
Ajith.KumarReddy <Ajith.KumarReddy@IN2270990W1.ey.net>
2021-05-14 10:49:25 +0530

    Step 10 commit added login service

--- spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/service/LoginService.java
+++ spring-boot-first-web-application/src/main/java/com/in28minutes/springboot/web/service/LoginService.java
@@ -3,6 +3,7 @@ package com.in28minutes.springboot.web.service;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 // @component says that this service is bean type and can be used for injecting in controllers
+//step10
 @Component
 public class LoginService {
     public boolean validateUser(String user, String password) {
